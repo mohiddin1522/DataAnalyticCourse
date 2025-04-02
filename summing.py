f4d3edffa6e11e5db5d4791c6607306f399c28de
mohiddin1522 <mohiddin2662@gmail.com>
2025-04-02 15:41:27 +0530

    python

---
+++ summing.py
@@ -0,0 +1,41 @@
+num1 = 20
+num2 = 10
+print(f"Sum of {num1} and {num2} is {num1 + num2}")
+# output
+#Sum of 20 and 10 is 30
+
+print(type(num1))
+
+# output
+# <class 'int'>
+
+name = "mohiddin"
+print(type(name))
+# output
+# <class 'str'>
+
+print(f"num1 + num2 = {num1 + num2}")
+print(f"num1 - num2 = {num1 - num2}")
+print(f"num1 * num2 = {num1 * num2}")
+print(f"num1 / num2 = {num1 / num2}")
+print(f"num1 // num2 = {num1 // num2}")
+print(f"num1 ** num2 = {num1 ** num2}")
+print(f"num1 % num2 = {num1 % num2}")
+
+# output
+# num1 + num2 = 30
+# num1 - num2 = 10
+# num1 * num2 = 200
+# num1 / num2 = 2.0
+# num1 // num2 = 2
+# num1 ** num2 = 10240000000000
+# num1 % num2 = 0 
+
+print("hello","world", sep="-")
+
+# output
+#hello-world
+
+print("hello","world", sep="=", end="!")
+# output
+#hello world!
